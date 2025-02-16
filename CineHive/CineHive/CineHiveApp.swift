cbdf5070e730119fcba1f8bc7071d047b05b97d9
Jong MIn <unib335@naver.com>
2025-02-16 19:19:50 +0900

    [Add]: 프로젝트 생성
    - SwiftUI 프로젝트 생성
    - 기본 설정 및 초기 파일 추가
    - Minimum Deployments iOS 17.0으로 설정

---
+++ CineHive/CineHive/CineHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineHiveApp.swift
+//  CineHive
+//
+//  Created by 이종민 on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CineHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
