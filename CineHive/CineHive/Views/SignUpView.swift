b73a5f8499409c9cfaa5f20c01d100c869b7ec21
EunJinCHO <till5941@gmail.com>
2025-02-18 22:57:46 +0900

    [Ref]: 회원가입 UI 리팩토링 및 입력 필드 컴포넌트화

--- CineHive/CineHive/Views/SignUpView.swift
+++ CineHive/CineHive/Views/SignUpView.swift
@@ -16,6 +16,8 @@ struct SignUpView: View {
     @State private var gender: String = ""
     @State private var selectedMan: Bool = false
     @State private var selectedWoman: Bool = false
+    @State private var showPassword: Bool = false
+
     
     var body: some View {
         VStack {
@@ -24,85 +26,10 @@ struct SignUpView: View {
                 .frame(width: 320, height: 70)
                 .font(.system(size: 22, weight: .bold))
             
-            VStack {
-                HStack {
-                        Text("이메일")
-                            .font(.system(size: 17))
-                        Text("*")
-                            .font(.system(size: 20))
-                            .foregroundStyle(.red)
-                            .offset(x: -7)
-                }
-                .frame(width: 320, height: 25, alignment: .leading)
-                
-                TextField("Email", text: $email)
-                    .frame(width: 300, height: 50)
-                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
-                    .frame(width: 330, height: 50)
-                    .overlay() {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color("FontColor"), lineWidth: 0.6)
-                    }
-            }
-            .frame(width: 330, height: 90)
-            
-            VStack {
-                HStack {
-                        Text("비밀번호")
-                            .font(.system(size: 17))
-                        Text("*")
-                            .font(.system(size: 20))
-                            .foregroundStyle(.red)
-                            .offset(x: -7)
-                }
-                .frame(width: 320, height: 25, alignment: .leading)
-                TextField("Password", text: $password)
-                    .frame(width: 300, height: 50)
-                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
-                    .frame(width: 330, height: 50)
-                    .overlay() {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color("FontColor"), lineWidth: 0.6)
-                    }
-            }
-            .frame(width: 330, height: 90)
-            
-            VStack {
-                HStack {
-                        Text("닉네임")
-                            .font(.system(size: 17))
-                        Text("*")
-                            .font(.system(size: 20))
-                            .foregroundStyle(.red)
-                            .offset(x: -7)
-                }
-                .frame(width: 320, height: 25, alignment: .leading)
-                
-                TextField("Nickname", text: $nickname)
-                    .frame(width: 300, height: 50)
-                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
-                    .frame(width: 330, height: 50)
-                    .overlay() {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color("FontColor"), lineWidth: 0.6)
-                    }
-            }
-            .frame(width: 330, height: 90)
-            
-            VStack {
-                Text("이름")
-                    .frame(width: 320, height: 25, alignment: .leading)
-                    .font(.system(size: 17))
-                TextField("Name", text: $name)
-                    .frame(width: 300, height: 50)
-                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
-                    .frame(width: 330, height: 50)
-                    .overlay() {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color("FontColor"), lineWidth: 0.6)
-                    }
-            }
-            .frame(width: 330, height: 90)
+            InputFieldView(title: "이메일", text: $email)
+            PasswordFieldView(title: "비밀번호", text: $password, showPassword: $showPassword)
+            InputFieldView(title: "닉네임", text: $nickname)
+            InputFieldView(title: "이름", text: $name)
             
             VStack {
                 Text("성별")
@@ -156,4 +83,78 @@ struct SignUpView: View {
 
 #Preview {
     SignUpView()
+}
+
+struct InputFieldView: View {
+    let title: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.system(size: 17))
+                Text("*")
+                    .font(.system(size: 20))
+                    .foregroundStyle(.red)
+                    .offset(x: -7)
+            }
+            .frame(width: 320, height: 25, alignment: .leading)
+            
+            TextField("Email", text: $text)
+                .frame(width: 300, height: 50)
+                .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                .frame(width: 330, height: 50)
+                .overlay() {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color("FontColor"), lineWidth: 0.6)
+                }
+        }
+        .frame(width: 330, height: 90)
+    }
+}
+
+struct PasswordFieldView: View {
+    let title: String
+    @Binding var text: String
+    @Binding var showPassword: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.system(size: 17))
+                Text("*")
+                    .font(.system(size: 20))
+                    .foregroundStyle(.red)
+                    .offset(x: -7)
+            }
+            .frame(width: 320, height: 25, alignment: .leading)
+            
+            HStack {
+                Section {
+                    if showPassword {
+                        TextField("Password", text: $text)
+                    } else {
+                        SecureField("Password", text: $text)
+                    }
+                }
+                .frame(width: 260, height: 40)
+                
+                Button(action: {
+                    self.showPassword.toggle()
+                }, label: {
+                    Image(systemName: showPassword ? "eye" : "eye.slash")
+                        .padding(.trailing, 1)
+                        .foregroundStyle(.gray)
+                })
+            }
+            .frame(width: 330, height: 50)
+            .overlay {
+                RoundedRectangle(cornerRadius: 13)
+                    .stroke(Color("FontColor"), lineWidth: 0.6)
+            }
+        }
+        .frame(width: 330, height: 90)
+    }
 }
