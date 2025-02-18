8d5ae2c496967cc0e9ff8dca4477019ff453d642
EunJinCHO <till5941@gmail.com>
2025-02-18 20:26:55 +0900

    [Add]: SignUpView 뷰 구현

--- CineHive/CineHive/Views/SignUpView.swift
+++ CineHive/CineHive/Views/SignUpView.swift
@@ -8,8 +8,127 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var nickname: String = ""
+    @State private var name: String = ""
+    @State private var gender: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Create Account")
+                .frame(width: 320, height: 70)
+                .font(.system(size: 22, weight: .bold))
+            
+            VStack {
+                HStack {
+                        Text("이메일")
+                            .font(.system(size: 17))
+                        Text("*")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.red)
+                            .offset(x: -7)
+                }
+                .frame(width: 320, height: 25, alignment: .leading)
+                
+                TextField("Email", text: $email)
+                    .frame(width: 300, height: 50)
+                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                    .frame(width: 330, height: 50)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color("FontColor"), lineWidth: 0.6)
+                    }
+            }
+            .frame(width: 330, height: 90)
+            
+            VStack {
+                HStack {
+                        Text("비밀번호")
+                            .font(.system(size: 17))
+                        Text("*")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.red)
+                            .offset(x: -7)
+                }
+                .frame(width: 320, height: 25, alignment: .leading)
+                TextField("Password", text: $password)
+                    .frame(width: 300, height: 50)
+                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                    .frame(width: 330, height: 50)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color("FontColor"), lineWidth: 0.6)
+                    }
+            }
+            .frame(width: 330, height: 90)
+            
+            VStack {
+                HStack {
+                        Text("닉네임")
+                            .font(.system(size: 17))
+                        Text("*")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.red)
+                            .offset(x: -7)
+                }
+                .frame(width: 320, height: 25, alignment: .leading)
+                
+                TextField("Nickname", text: $nickname)
+                    .frame(width: 300, height: 50)
+                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                    .frame(width: 330, height: 50)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color("FontColor"), lineWidth: 0.6)
+                    }
+            }
+            .frame(width: 330, height: 90)
+            
+            VStack {
+                Text("이름")
+                    .frame(width: 320, height: 25, alignment: .leading)
+                    .font(.system(size: 17))
+                TextField("Name", text: $name)
+                    .frame(width: 300, height: 50)
+                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                    .frame(width: 330, height: 50)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color("FontColor"), lineWidth: 0.6)
+                    }
+            }
+            .frame(width: 330, height: 90)
+            
+            VStack {
+                Text("성별")
+                    .frame(width: 320, height: 25, alignment: .leading)
+                    .font(.system(size: 17))
+                TextField("Gender", text: $gender)
+                    .frame(width: 300, height: 50)
+                    .textInputAutocapitalization(.never)    // 첫 글자 대문자 표출 X
+                    .frame(width: 330, height: 50)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color("FontColor"), lineWidth: 0.6)
+                    }
+            }
+            .frame(width: 330, height: 90)
+            
+            Button(action: {
+                print("gd")
+            }, label: {
+                Text("회원가입")
+                    .frame(width: 330, height: 50)
+                    .background(Color("LoginBtnColor")).clipShape(RoundedRectangle(cornerRadius: 12))
+            })
+            .frame(height: 90)
+            .font(.system(size: 18, weight: .bold))
+            .foregroundStyle(.white)
+            Spacer()
+        }
     }
 }
 
