1d0de1d2b6aed0da35642114be95ad6aa0198bba
Jong MIn <unib335@naver.com>
2025-02-22 15:43:42 +0900

    [Add]: DetailView 추가
    - 영화 상세 정보를 표시할 DetailView 파일 생성

---
+++ CineHive/CineHive/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CineHive
+//
+//  Created by 이종민 on 2/22/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
