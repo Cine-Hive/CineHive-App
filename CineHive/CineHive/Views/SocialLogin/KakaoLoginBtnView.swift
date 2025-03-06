dcdbc1689c96142ce281db641ecdf5f84ecd59ca
EunJinCHO <till5941@gmail.com>
2025-03-06 10:32:23 +0900

    [Add]: 로그인 방식 분기
    - 카카오톡(앱) 로그인, 카카오 계정 로그인 분기 처리 추가
    - Task 활용해 로그인 후 추가 요청 처리

--- CineHive/CineHive/Views/SocialLogin/KakaoLoginBtnView.swift
+++ CineHive/CineHive/Views/SocialLogin/KakaoLoginBtnView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import KakaoSDKUser
 
 struct KakaoLoginBtnView: View {
     
@@ -13,8 +14,33 @@ struct KakaoLoginBtnView: View {
     
     var body: some View {
         Button(action: {
-            Task {
-                await viewModel.kakaologin()
+            // 카카오톡 실행 가능 여부 확인
+            if UserApi.isKakaoTalkLoginAvailable() {
+                // 카카오톡 로그인
+                UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("카카오톡 로그인 success")
+                        
+                        // 추가작업
+                        _ = oauthToken
+                    }
+                }
+            } else {
+                // 카카오계정 로그인
+                UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        Task {
+                            await viewModel.kakaologin()
+                        }
+                        print("카카오계정 로그인 success")
+                        // 추가작업
+                        _ = oauthToken
+                    }
+                }
             }
         }, label: {
             HStack {
