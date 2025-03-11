e1fb66e14e39c04d60298c1493c9534165e75bb5
EunJinCHO <till5941@gmail.com>
2025-03-11 09:43:20 +0900

    [Add]: AdditionalInfoView 추가
    - 로그인 후 사용자 추가 정보 입력 페이지 추가

---
+++ CineHive/CineHive/Views/AdditionalInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  AdditionalInfoView.swift
+//  CineHive
+//
+//  Created by 존진 on 3/10/25.
+//
+
+import SwiftUI
+
+struct AdditionalInfoView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    AdditionalInfoView()
+}
