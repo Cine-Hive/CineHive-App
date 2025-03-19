fad5d02acdf9c24ef4cd66957ef46519e062a4af
Jong MIn <unib335@naver.com>
2025-03-20 00:22:13 +0900

    [Ref]: 더미파일들 Dummy 폴더로 이동

---
+++ CineHive/CineHive/Resources/Dummy/MovieDetail+Dummy.swift
@@ -0,0 +1,78 @@
+//
+//  MovieDetail+Dummy.swift
+//  CineHive
+//
+//  Created by 이종민 on 3/4/25.
+//
+
+import Foundation
+
+// MARK: - Actor 더미 데이터
+extension Actor {
+    static var dummy: Actor {
+        Actor(
+            id: 1,
+            name: "홍길동",
+            posterPath: "/dummyActor1.jpg"
+        )
+    }
+    
+    static var dummy2: Actor {
+        Actor(
+            id: 2,
+            name: "김철수",
+            posterPath: "/dummyActor2.jpg"
+        )
+    }
+}
+
+// MARK: - Genre 더미 데이터
+extension Genre {
+    static var dummy1: Genre {
+        Genre(id: 28, name: "액션")
+    }
+    
+    static var dummy2: Genre {
+        Genre(id: 12, name: "모험")
+    }
+    
+    static var dummy3: Genre {
+        Genre(id: 16, name: "애니메이션")
+    }
+}
+
+
+
+// MARK: - Director 더미 데이터
+extension Director {
+    static var dummy: Director {
+        Director(
+            id: 1,
+            name: "이종민 감독"
+        )
+    }
+}
+
+// MARK: - MovieDetail 더미 데이터
+extension MovieDetail {
+    static var dummy: MovieDetail {
+        MovieDetail(
+            id: 1,
+            title: "더미 영화 제목",
+            overview: """
+            이 영화는 예시로 사용되는 더미 데이터입니다. 영화의 줄거리와 설명이 포함되어 있으며, 
+            실제 서비스에서 사용할 데이터와 유사한 형식으로 작성되었습니다.
+            """,
+            posterPath: "/iraQz6gdAe8JL45QcBifM1UhQ38.jpg",
+            backDropPath: "/mzfx54nfDPTUXZOG48u4LaEheDy.jpg",
+            releaseDate: "2025-02-28",
+            genres: [Genre.dummy1, Genre.dummy2, Genre.dummy3],
+            voteAverage: 7.8,
+            popularity: 1234.5,
+            actors: [Actor.dummy, Actor.dummy2],
+            videos: [Video.dummy, Video.dummy2, Video.dummy3, Video.dummy4, Video.dummy5],
+            director: Director.dummy,
+            runtime: 120
+        )
+    }
+}
