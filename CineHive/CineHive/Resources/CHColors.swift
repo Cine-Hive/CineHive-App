c98328d8731c4807f4f97bf7eebf758ddac4c945
Jong MIn <unib335@naver.com>
2025-03-21 04:39:47 +0900

    [Fix]: `CHColors.secondaryColor` 색상 정리

--- CineHive/CineHive/Resources/CHColors.swift
+++ CineHive/CineHive/Resources/CHColors.swift
@@ -12,13 +12,14 @@ struct CHColors {
     static let primaryColor = Color("PrimaryColor")
     static let backgroundColor = Color("BackgroundColor")
     static let textColor = Color("TextColor")
-    static let secondaryColor = Color("SecondaryColor")
+    static let secondaryColor = Color.gray
     static let font = Color("FontColor")
     
     static let inputBackground = Color(white: 0.15)
     static let cardBackground = Color(white: 0.15)
     static let starColor = Color.yellow
     static let divider = Color.gray.opacity(0.3)
+    static let gray = Color.gray
     
     struct Button {
         static let primary = Color("LoginBtnColor") // 또는 Color(hex: "#FF2F6E")
