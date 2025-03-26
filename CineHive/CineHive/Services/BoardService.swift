c5734909dbbf877968594cc50ec12beeecdbdc82
Jong MIn <unib335@naver.com>
2025-03-26 21:30:09 +0900

    [Add]: 게시글 목록 조회 기능 추가
    - BoardService에 전체 게시글 조회 메서드 fetchBoards() 추가
    - API 엔드포인트 "/boards"를 통해 게시글 리스트 데이터를 비동기로 가져옴

--- CineHive/CineHive/Services/BoardService.swift
+++ CineHive/CineHive/Services/BoardService.swift
@@ -5,18 +5,17 @@
 //  Created by 이종민 on 3/26/25.
 //
 
-
 import Foundation
 
 final class BoardService {
     static let shared = BoardService()
     private init() { }
-
+    
     private let commentEndpoint = "/comment/all/board"
-
-    /// boardId에 해당하는 게시글의 전체 댓글을 조회합니다.
-    func fetchComments(for boardId: Int) async throws -> [Comment] {
-        let endpoint = "\(commentEndpoint)/\(boardId)"
-        return try await NetworkManager.shared.fetch(endpoint: endpoint)
+    private let boardsEndpoint = "/boards"
+    
+    /// 전체 게시글 목록을 조회합니다.
+    func fetchBoards() async throws -> [Board] {
+        return try await NetworkManager.shared.fetch(endpoint: boardsEndpoint)
     }
 }
