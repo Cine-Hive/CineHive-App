229eac1082a8fcedfc04435e98ed017df2a9ea90
Jong MIn <unib335@naver.com>
2025-03-27 06:02:00 +0900

    [Add]: CommentViewModel 생성
    - 댓글 관련 비즈니스 로직을 관리할 CommentViewModel 생성

---
+++ CineHive/CineHive/ViewModels/CommentViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CommentViewModel.swift
+//  CineHive
+//
+//  Created by 이종민 on 3/27/25.
+//
+
+import Foundation
+import Observation
+
+@Observable
+final class CommentViewModel {
+    
+}
