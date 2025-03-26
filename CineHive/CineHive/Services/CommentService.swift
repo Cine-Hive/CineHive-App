2e842c244105f0efac41a7c7aeb9488233d8f491
Jong MIn <unib335@naver.com>
2025-03-27 05:26:41 +0900

    [Add]: CommentService 클래스 추가
    - 댓글 관련 API 기능을 담당할 싱글톤 서비스 클래스 생성
    - 추후 댓글 조회, 작성, 삭제 등의 기능을 이 클래스에 구현 예정

---
+++ CineHive/CineHive/Services/CommentService.swift
@@ -0,0 +1,15 @@
+//
+//  CommentService.swift
+//  CineHive
+//
+//  Created by 이종민 on 3/27/25.
+//
+
+import Foundation
+
+final class CommentService {
+    static let shared = CommentService()
+    private init() { }
+    
+    
+}
