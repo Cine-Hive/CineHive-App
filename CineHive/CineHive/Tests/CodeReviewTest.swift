841a42b9a2df9edf8ae191f365a4480e6b4642e6
Jong MIn <unib335@naver.com>
2025-04-10 00:08:01 +0900

    [Add]: 코드 리뷰 테스트

---
+++ CineHive/CineHive/Tests/CodeReviewTest.swift
@@ -0,0 +1,20 @@
+//
+//  CodeReviewTest.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/10/25.
+//
+
+import SwiftUI
+
+struct CodeReviewTest: View {
+    var body: some View {
+        Text("Code Reveiw 테스트")
+        Text("코드 리뷰 테스트")
+        
+    }
+}
+
+#Preview {
+    CodeReviewTest()
+}
