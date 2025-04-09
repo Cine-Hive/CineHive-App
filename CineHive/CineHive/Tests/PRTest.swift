f43fa1e8174bb245ce70b37d66d54359be5271e3
Jong MIn <unib335@naver.com>
2025-04-09 22:33:12 +0900

    [Add]: PR 테스트용 뷰 추가
    - PR 동작 테스트를 위해 PRTest.swift 파일 생성
    - 단순 텍스트 뷰를 반복하여 출력하는 샘플 뷰 구성

---
+++ CineHive/CineHive/Tests/PRTest.swift
@@ -0,0 +1,22 @@
+//
+//  PRTest.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/9/25.
+//
+
+import SwiftUI
+
+struct PRTest: View {
+    var body: some View {
+        Text("PR테스트용 뷰를 작성하였습니다.")
+        Text("PR테스트용 뷰를 작성하였습니다.")
+        Text("PR테스트용 뷰를 작성하였습니다.")
+        Text("PR테스트용 뷰를 작성하였습니다.")
+        Text("PR테스트용 뷰를 작성하였습니다.")
+    }
+}
+
+#Preview {
+    PRTest()
+}
