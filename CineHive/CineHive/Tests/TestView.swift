1ecae7b6466c62acb44a6e25b4c67cf7389b415d
Jong MIn <unib335@naver.com>
2025-04-10 01:22:55 +0900

    [Add] 테스트용 SwiftUI View(TestView) 추가
    - 테스트 목적의 TestView.swift 생성
    - 기본 Text 뷰 구성: "TestView", "테스트용도입니다." 출력
    - PreviewProvider 포함

---
+++ CineHive/CineHive/Tests/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/10/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("TestView")
+        Text("테스트용도입니다.")
+    }
+}
+
+#Preview {
+    TestView()
+}
