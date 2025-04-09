0c488ddfaff23343ee2546cbb682287b665a54de
Jong MIn <unib335@naver.com>
2025-04-10 01:14:34 +0900

    [Add] 테스트용 TestCode 뷰 추가
    - SwiftUI 기반의 테스트용 `TestCode` 뷰 생성
    - Preview 포함, UI 테스트 및 개발 중 확인용 용도

---
+++ CineHive/CineHive/Tests/TestCode.swift
@@ -0,0 +1,22 @@
+//
+//  TestCode.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/10/25.
+//
+
+import SwiftUI
+
+struct TestCode: View {
+    var body: some View {
+        Text("this is test code")
+        Text("this is test code")
+        Text("this is test code")
+        Text("this is test code")
+        Text("this is test code")
+    }
+}
+
+#Preview {
+    TestCode()
+}
