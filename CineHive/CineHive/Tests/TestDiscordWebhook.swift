3154dfbefe1989beaa732d9542713f5b38695f97
Jong MIn <unib335@naver.com>
2025-04-10 02:26:16 +0900

    [Test] Discord 웹훅 연동 테스트용 뷰 추가
    - 테스트용 SwiftUI 뷰 `TestDiscordWebhook` 생성
    - Discord 웹훅 기능 동작 확인 목적

---
+++ CineHive/CineHive/Tests/TestDiscordWebhook.swift
@@ -0,0 +1,18 @@
+//
+//  TestDiscordWebhook.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/10/25.
+//
+
+import SwiftUI
+
+struct TestDiscordWebhook: View {
+    var body: some View {
+        Text("웹훅을 위한 테스트입니다.")
+    }
+}
+
+#Preview {
+    TestDiscordWebhook()
+}
