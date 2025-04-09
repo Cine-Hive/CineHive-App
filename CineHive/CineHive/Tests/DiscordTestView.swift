d5b2997015039a40b4c8151dba3cb89b61221efa
Jong MIn <unib335@naver.com>
2025-04-10 02:36:46 +0900

    [Add]: TestView 추가
    - 디스코드 알림 테스트

---
+++ CineHive/CineHive/Tests/DiscordTestView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscordTestView.swift
+//  CineHive
+//
+//  Created by 이종민 on 4/10/25.
+//
+
+import SwiftUI
+
+struct DiscordTestView: View {
+    var body: some View {
+        Text("Test View")
+    }
+}
+
+#Preview {
+    DiscordTestView()
+}
