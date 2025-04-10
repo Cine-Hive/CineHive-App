b7a47245164b5707904d7952951c8d420c5cb648
EunJinCHO <till5941@gmail.com>
2025-04-10 11:13:30 +0900

    [Add]: 카카오 SDK 초기화 코드 작성

--- CineHive/CineHive/App/CineHiveApp.swift
+++ CineHive/CineHive/App/CineHiveApp.swift
@@ -6,15 +6,30 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct CineHiveApp: App {
     @State private var userState = UserState.shared
     
+    init() {
+        // Kakao SDK 초기화
+        if let kakaoAppKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String {
+            KakaoSDK.initSDK(appKey: kakaoAppKey)
+            print("Kakao App Key: \(kakaoAppKey)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(userState)
+            // onOpenURL()을 사용해 커스텀 URL 스킴 처리
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    AuthController.handleOpenUrl(url: url)
+                }
+            })
+            .environment(userState)
         }
     }
 }
