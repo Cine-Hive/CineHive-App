0936b8a733b721c6f3e3c87dbcd9a771fdb4caef
EunJinCHO <till5941@gmail.com>
2025-04-10 23:40:22 +0900

    [Add]: 카카오 로그인 기능 구현
    - 카카오톡 설치 여부에 따른 로그인 방식 분기
    - 로그인 성공 시 accessToken으로 서버에 로그인 요청 코드 작성

---
+++ CineHive/CineHive/Features/Auth/ViewModel/KakaoLoginViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  KakaoLoginViewModel.swift
+//  CineHive
+//
+//  Created by 존진 on 4/10/25.
+//
+
+import Foundation
+import UIKit
+import OSLog
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
+
+@Observable
+class KaKaoLoginViewModel {
+    
+    private let userService: UserService
+    
+    init(userService: UserService = .shared) {
+        self.userService = userService
+    }
+    
+    func login() {
+        // 카카오톡 설치 여부 확인
+        if UserApi.isKakaoTalkLoginAvailable() {
+            // 카카오톡 앱을 통한 로그인
+            UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                if let error = error {
+                    Logger.log(.error, category: Logger.auth, message: "카카오톡 로그인 실패: \(error.localizedDescription)")
+                } else if let token = oauthToken {
+                    Task {
+                        do {
+                            let response = try await self.userService.kakaoLogin(token: token.accessToken)
+                        } catch {
+                            print("서버 로그인 실패: \(error.localizedDescription)")
+                        }
+                    }           
+                    self.getUserInfo()
+                }
+            }
+        } else {
+            // 카카오 계정으로 로그인 (웹뷰 방식)
+            UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+                if let error = error {
+                    Logger.log(.error, category: Logger.auth, message: "카카오톡 로그인 실패: \(error.localizedDescription)")
+                } else if let token = oauthToken {
+                    Task {
+                        do {
+                            let response = try await self.userService.kakaoLogin(token: token.accessToken)
+                        } catch {
+                            Logger.log(.error, category: Logger.auth, message:"서버 로그인 실패: \(error.localizedDescription)")
+                        }
+                    }
+                    self.getUserInfo()
+                }
+            }
+        }
+    }
+  
+    private func getUserInfo() {
+        UserApi.shared.me() { (user, error) in
+            if let error = error {
+                Logger.log(.info, category: Logger.state, message: "사용자 정보 가져오기 실패: \(error.localizedDescription)")
+            } else {
+                if let user = user {
+                    print("id: \(user.id ?? 0)")
+                    print("nickname: \(user.kakaoAccount?.profile?.nickname ?? "")")
+                    print("email: \(user.kakaoAccount?.email ?? "")")
+                }
+            }
+        }
+    }
+}
