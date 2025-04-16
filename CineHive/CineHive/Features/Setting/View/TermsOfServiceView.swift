68eb0e073fb9adf2d8a9b4d3fdef430f7e081c43
Jong MIn <unib335@naver.com>
2025-04-17 02:29:27 +0900

    [Add]: dismiss 및 컬러스키마 상태 정의
    - 상태 정의 및 사용할 색상 정의

--- CineHive/CineHive/Features/Setting/View/TermsOfServiceView.swift
+++ CineHive/CineHive/Features/Setting/View/TermsOfServiceView.swift
@@ -8,4 +8,17 @@
 import SwiftUI
 
 struct TermsOfServiceView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.colorScheme) private var colorScheme
+    
+    private let backgroundColor = CHColors.backgroundColor
+    private let textColor = CHColors.textColor
+    private let secondaryColor = CHColors.secondaryColor
+    
+    var body: some View {
+}
+
+#Preview {
+    TermsOfServiceView()
+        .preferredColorScheme(.dark)
 }
