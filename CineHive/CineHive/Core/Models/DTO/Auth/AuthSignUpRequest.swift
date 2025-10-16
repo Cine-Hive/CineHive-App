5ec22e3b55706524983c21001b738017ce72c9e7
EunJinCHO <till5941@gmail.com>
2025-10-16 15:53:04 +0900

    [Ref]: AuthSignUpRequest DTO 구조 개선
    - Supabase SDK가 요구하는 [String: AnyJSON] 포맷 변경
    - DTO 내부에서 JSON 변환 로직을 처리하여 ViewModel, Service 단의 중복 제거

--- CineHive/CineHive/Core/Models/DTO/Auth/AuthSignUpRequest.swift
+++ CineHive/CineHive/Core/Models/DTO/Auth/AuthSignUpRequest.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import Supabase
 
 // 소셜 회원 가입 요청 DTO
 struct AuthSignUpRequest: Encodable {
     let email: String
     let password: String
+    let data: [String: AnyJSON]
+    
+    init(email: String, password: String, nickname: String) {
+        self.email = email
+        self.password = password
+        self.data = [
+            "nickname": .string(nickname)
+        ]
+    }
 }
